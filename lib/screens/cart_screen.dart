23541f86b39e76fa2a9c8c7a471aca2413fff59e
Abubakr Muhammad <realchampiontheno1@gmail.com>
2022-07-28 09:42:25 +0500

    add confirmation dialog on removing cart item

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -87,6 +87,35 @@ class _CartItem extends StatelessWidget {
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(cartItem.id);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: const Text('Are you sure?'),
+                  content: const Text(
+                      'Do you want to remove the item from the cart?'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop(false);
+                      },
+                      child: const Text(
+                        'No',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(ctx).pop(true);
+                      },
+                      child: const Text(
+                        'Yes',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  ],
+                ));
+      },
       background: Container(
         alignment: Alignment.centerRight,
         decoration: BoxDecoration(
